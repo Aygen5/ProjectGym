e3a9b56bc8695a4f4927a22c5511748fe4c4ee9a
Aygen5 <aygenyildirim27@gmail.com>
2026-07-18 16:44:49 +0300

    feat(infrastructure): add database initializer

--- src/ProjectGym.API/Program.cs
+++ src/ProjectGym.API/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Identity;
 using ProjectGym.API.Extensions;
+using ProjectGym.Infrastructure.Data;
+using ProjectGym.Infrastructure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +18,21 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapOpenApi();
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    try
+    {
+        var context = services.GetRequiredService<ProjectGymDbContext>();
+        var userManager = services.GetRequiredService<UserManager<AppIdentityUser>>();
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+        await DbInitializer.SeedAsync(context,userManager,roleManager,logger);
+    }
+    catch (Exception ex)
+    {
+        logger.LogError(ex, $"Seed işlemi sırasında bir hata oluştu: {ex.Message}");
+    }
 }
 
 app.UseHttpsRedirection();
